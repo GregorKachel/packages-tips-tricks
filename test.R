19233182a10cbe7ac993143fdb7e48a92f1ccc16
RamiroG <glauer@fh-potsdam.de>
2019-02-14 22:38:26 +0100

    Update test.R

--- test.R
+++ test.R
@@ -1,1 +1,11 @@
 ### test test test ###
+
+name <- c('Gregor', 'Andreas', 'Ramiro')
+hairfactor <- c(20, 2, 5)
+legs <- c(2, 2, 2)
+arms <- c(2, 2, 2)
+kids <- c(1, 0, 2)
+
+TestDF <- data.frame(name, hairfactor, legs, arms, kids)
+
+
