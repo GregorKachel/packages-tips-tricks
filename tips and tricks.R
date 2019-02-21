ca5a079bdc251ec27fedf8319a4174834a2b8d01
GregorKachel <33640099+GregorKachel@users.noreply.github.com>
2019-02-21 08:45:52 +0100

    Create tips and tricks.R

---
+++ tips and tricks.R
@@ -0,0 +1,14 @@
+
+
+### Tipps Tricks and maybe useful packages
+
+
+
+
+
+### Package für descritive summary statistics:
+# descriptr
+# https://www.r-bloggers.com/descriptive-summary-statistics-with-descriptr/
+
+
+
